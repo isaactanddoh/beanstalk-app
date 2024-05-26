5f773d54e492c169fe8b478e559e2b554aa242e0
isaactanddoh <isaactanddoh@gmail.com>
2024-05-26 21:33:00 +0000

    Added HTML tags

--- app/Program.cs
+++ app/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "<h1>I can now deploy applications to Beanstalk Envs. Thanks to JOMACS!!!</h1>");
+app.MapGet("/", (HttpContext context) => 
+{
+    context.Response.ContentType = "text/html";
+    return "<h1>I can now deploy applications to Beanstalk Envs. Thanks to JOMACS!</h1>";
+});
 
 app.Run();
