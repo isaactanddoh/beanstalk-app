567aa56126de0069f476401fe87a3b567b23f7df
isaactanddoh <isaactanddoh@gmail.com>
2024-05-26 21:26:32 +0000

    Edited the Program.CS file

--- app/Program.cs
+++ app/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "I can now deploy applications to Beanstalk Envs. Thanks to JOMACS!");
+app.MapGet("/", () => "<h1>I can now deploy applications to Beanstalk Envs. Thanks to JOMACS!</h1>");
 
 app.Run();
